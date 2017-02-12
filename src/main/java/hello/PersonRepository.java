c9fa92538b07c228bf2272a8e33f44d6bb694acb
hao-private <hao.yu.de@gmail.com>
2017-02-12 17:18:30 +0100

    add security to rest api

--- src/main/java/hello/PersonRepository.java
+++ src/main/java/hello/PersonRepository.java
@@ -1,8 +1,10 @@
 package hello;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface PersonRepository extends CrudRepository<Person, Long>{
-	
+	Person findByName(String name);
 }
--- src/main/java/hello/PersonsController.java
+++ src/main/java/hello/PersonsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ public class PersonsController {
 	PersonRepository repo;
 	
 	@RequestMapping(method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public Collection<Person> getPersons() {
 		Collection<Person> persons = new ArrayList<Person>();
 		repo.findAll().forEach(persons::add);
@@ -26,6 +28,7 @@ public class PersonsController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public ResponseEntity<?> addKid(@RequestBody Person person) {
 		repo.save(person);
 		HttpHeaders httpHeaders = new HttpHeaders();
